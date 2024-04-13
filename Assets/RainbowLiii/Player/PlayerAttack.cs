49b043c6d4d5adf79752baf0d4b712158ddbb1a5
lrbjk <118360194+lrbjk@users.noreply.github.com>
2024-04-13 21:57:29 +0800

    实现攻击

--- Assets/RainbowLiii/Player/PlayerAttack.cs
+++ Assets/RainbowLiii/Player/PlayerAttack.cs
@@ -4,15 +4,36 @@ using UnityEngine;
 
 public class PlayerAttack : MonoBehaviour
 {
-    public 
+    public GameObject Microphone;
+    private MicrophoneInput MI;
+    public CircleCollider2D circle;
+    public float MaxRange;
+    public float MaxTime;
+    public float esctime;
     // Start is called before the first frame update
     void Start()
     {
-        
+        MI = Microphone.GetComponent<MicrophoneInput>();
+        circle = GetComponent<CircleCollider2D>();
+        esctime = MaxTime;
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
+    {
+        if(esctime >= MaxTime)
+        {
+            circle.radius = MI.realVolume / MaxRange;
+            circle.enabled = true;
+            esctime = 0f;
+        }
+        else
+        {
+            esctime += Time.deltaTime;
+            circle.enabled = false;
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
     {
         
     }
